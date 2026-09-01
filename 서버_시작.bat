ae3a32cfa13e78a0733a6815e025d4b0cda62b0b
bsh3408 <bsh3408@users.noreply.github.com>
2026-09-01 13:05:38 +0900

    배치파일에서 한글 echo 문구를 완전히 제거해 인코딩 오류 원인 제거

--- 서버_시작.bat
+++ 서버_시작.bat
@@ -1,7 +1,6 @@
 @echo off
 chcp 65001 > nul
 cd /d "%~dp0"
-echo 통합과학연구소 서버를 시작합니다. 이 창을 닫으면 서버도 꺼집니다.
 start http://localhost:5173/교사용_수행평가관리.html
 "C:\Program Files\nodejs\node.exe" serve.js "%~dp0"
 pause
